383bedaa43084c282e248ea0e3e0e480b9686052
Igho <reffreily5000@gmail.com>
2024-06-28 16:38:19 +0100

    create and styling food details component

--- src/component/FoodDetails.jsx
+++ src/component/FoodDetails.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import styles from "./fooddetails.module.css"
 
 export default function FoodDetails({foodId}){
     
@@ -6,6 +7,7 @@ export default function FoodDetails({foodId}){
     const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
 
     const [food, setFood] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     //syntax of useEffect hook. pass a callback function ()=>{} and dependency array []
     useEffect(() => {
@@ -16,6 +18,7 @@ export default function FoodDetails({foodId}){
            const data = await res.json();
            console.log(data);
            setFood(data);
+           setIsLoading(false);
         }
 
         //calling of the function
@@ -24,6 +27,43 @@ export default function FoodDetails({foodId}){
     }, [foodId]);
 
     return(
-        <div>Food Details {foodId} {food.title} <img src={food.image}/> </div>
+        <div>
+            <div className={styles.receipeCard}>
+                
+                <h1 className={styles.receipeName}>{food.title}</h1>
+                <img className={styles.receipeImage} src={food.image}/>
+                
+                <div className={styles.receipeDetails}> 
+                    <span>
+                        🕧 <strong>{food.readyInMinutes} Minutes</strong>
+                    </span>
+
+                    <span>
+                        👨‍👨‍👧‍👧<strong> Serves {food.servings}</strong>
+                    </span> 
+
+                    <span>
+                        <strong>{food.vegetarian ? "🥕 Vegetarian" : "🥩 Non-Vegetarian"}</strong>
+                    </span>
+                    <span>
+                        <strong>{food.vegan ? " 🐮 Vegan" : ""}</strong>
+                    </span>
+                </div>
+
+                <div>
+                    <span>
+                        💲 <strong>{food.pricePerServing/100} per serving </strong>
+                    </span>
+                </div>
+
+                <h2>Instructions</h2>
+                <div className={styles.receipeInstructions}>
+                    <ol>
+                        {isLoading?<p>Loading...</p> : <p>{food.analyzedInstructions[0].steps.map((item)=>(<li key={item.number}>{item.step}</li>))}</p>}
+                    </ol>
+                </div>
+
+            </div>
+        </div>
     );
 }
